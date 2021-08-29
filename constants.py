a8031542f8cec88bd27c5fcfc4bd4a132493da39
hajesha <32684196+hajesha@users.noreply.github.com>
2021-08-29 03:32:07 -0400

    collect data on a single page
    collect data on the ratings, warning, category, isWip, and the pairs

---
+++ constants.py
@@ -0,0 +1,74 @@
+id_to_rating = {
+  "9": "NR",
+  "10": "G",
+  "11": "T",
+  "12": "M",
+  "13": "E"
+}
+
+
+id_to_category = {
+  "116": "FF",
+  "23": "MM",
+  "2246": "Multi",
+  "22": "FM",
+  "21": "Gen",
+  "24": "Other"
+}
+
+id_to_warning = {
+  "16": "NAWA",
+  "19": "RNC",
+  "18": "MCD",
+  "14": "CNTUAW",
+  "20": "Underage",
+  "17": "GDOV"
+}
+
+category_to_abbreviation = {
+  "FF": "F/F",
+  "MM": "M/M",
+  "FM": "F/M",
+  "Gen": "Gen",
+  "Multi": "Multi",
+  "Other": "Other"
+}
+
+
+category_to_id = {
+  "FF": 116,
+  "MM": 23,
+  "F/F": 116,
+  "M/M": 23,
+  "Multi": 2246,
+  "F/M": 22,
+  "FM": 22,
+  "Gen": 21,
+  "Other": 24
+}
+
+
+warning_to_id = {
+  "No Archive Warnings Apply": 16,
+  "NAWA": 16,
+  "Rape/Non-Con": 19,
+  "RNC": 19,
+  "Major Character Death": 18,
+  "MCD": 18,
+  "Choose Not To Use Archive Warnings": 14,
+  "CNTUAW": 14,
+  "Underage": 20,
+  "Graphic Depictions Of Violence": 17,
+  "GDOV": 17
+}
+
+warning_to_abbr = {
+  "No Archive Warnings Apply": "NAWA",
+  "Rape/Non-Con": "RNC",
+  "Major Character Death": "MCD",
+  "Choose Not To Use Archive Warnings": "CNTUAW",
+  "Underage": "Underage",
+  "Graphic Depictions Of Violence": "GDOV"
+}
+
+domain = "https://archiveofourown.org/"
--- main.py
+++ main.py
@@ -1,6 +1,87 @@
-def print_hi(name):
-    print(f'Hi, {name}')
+import constants
+import requests
+import urllib.parse
+from bs4 import BeautifulSoup
+from collections import defaultdict
+
+class Story(object):
+    rating = ""
+    warning = 0
+    category = ""
+    iswip = True
+    relationships = []
+    freeforms = []
+    desc = ""
+
+    # The class "constructor" - It's actually an initializer
+    def __init__(self, rating, warning, category, iswip, relationships, freeforms, desc):
+        self.rating = rating
+        self.warning = warning
+        self.warning = warning
+        self.category = category
+        self.iswip = iswip
+        self.relationships = relationships
+        self.freeforms = freeforms
+        self.desc = desc
+
+
+def getWebpage(url):
+    webpage = requests.get(url)
+
+    if webpage.status_code == 200:
+        pass
+
+    # TODO: partial load
+    elif webpage.status_code == 206:
+        pass
+    else:
+        print(f"Error Code: {webpage.status_code}\nUrl: {url}\n------------\n")
+        return None
+
+    return BeautifulSoup(webpage.content, "html.parser")
+
+
+def createUrl(base, slug, queryparams):
+    return base + "/" + "/".join(slug) + "?" + urllib.parse.urlencode(queryparams)
+
+
+def getInformation(webpage, juststats=True, rating=True, warning=True, category=True, iswip=True, relationships=True,
+                   characters=True, freeforms=True):
+    info = dict()
+    #listOfStories = [] #TODO: add the stories into a container
+    ratingDict = defaultdict(int)
+    warningDict = defaultdict(int)
+    categoryDict = defaultdict(int)
+    wipDict = defaultdict(int)
+    pairsDict = defaultdict(int)
+
+    workSet = webpage.findAll('li', class_='work')
+    for work in workSet:
+        if rating:
+            ratingDict[work.find('span', class_="rating").attrs['class'][0]] += 1
+        if warning:
+            warningDict[work.find('span', class_="warnings").attrs['class'][0]] += 1
+        if category:
+            for i in work.find('span', class_="category")['title'].split(","):
+                categoryDict[i.strip()] += 1
+        if iswip:
+            wipDict[work.find('span', class_="iswip").attrs['class'][0]] += 1
+        if relationships:
+            pairs = work.findAll('li', class_="relationships")
+            # (Optional) This should be parsed to accomodate the order of the pairing
+            #ex Person A | Person B or Person B | Person A
+            for i in pairs:
+                pairsDict[i.text] += 1
+
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
 
+    # Todo: Remove as this is for dev purposes
+    slug = ["tags", "Beyblade", "works"]
+    queryparams = { "page": 1}
+
+    url = createUrl(constants.domain, slug, queryparams)
+    webpage = getWebpage(url)
+
+    if webpage:
+        getInformation(webpage)
